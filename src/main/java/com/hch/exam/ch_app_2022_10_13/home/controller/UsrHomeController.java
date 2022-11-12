6916c5c9925630a9a46c330adea988baa5f3f2dc
hwnagchangho <changho3755@gmail.com>
2022-11-12 14:28:54 +0900

    24강, 게시물 컨트롤러에, 게시물 추가, 리스트 기능 구현

--- src/main/java/com/hch/exam/ch_app_2022_10_13/home/controller/UsrHomeController.java
+++ src/main/java/com/hch/exam/ch_app_2022_10_13/home/controller/UsrHomeController.java
@@ -1,8 +1,6 @@
 package com.hch.exam.ch_app_2022_10_13.home.controller;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.hch.exam.ch_app_2022_10_13.article.vo.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,69 +21,41 @@ public class UsrHomeController {
   public int getCount() {
     return count;
   }
-
   @RequestMapping("/usr/home/getString")
   @ResponseBody
   public String getString() {
     return "HI";
   }
-
   @RequestMapping("/usr/home/getInt")
   @ResponseBody
   public int getInt() {
     return 10;
   }
-
   @RequestMapping("/usr/home/getFloat")
   @ResponseBody
   public float getFloat() {
     return 10.5f;
   }
-
   @RequestMapping("/usr/home/getDouble")
-  @ResponseBody
-  public double getDouble() {
-    return 10.5;
-  }
-
-  @RequestMapping("/usr/home/getBoolean")
-  @ResponseBody
-  public boolean getBoolean() {
-    return true;
-  }
-
-  @RequestMapping("/usr/home/getCharacter")
-  @ResponseBody
-  public char getCharacter() {
-    return 'a';
-  }
-
-  @RequestMapping("/usr/home/getList")
   @ResponseBody
   public List<String> getList() {
     List<String> list = new ArrayList<>();
     list.add("철수");
     list.add("영희");
-
     return list;
   }
-
-
   @RequestMapping("/usr/home/getMap")
   @ResponseBody
   public Map<String, Object> getMap() {
     Map<String, Object> map = new HashMap<>();
     map.put("철수나이", 22);
     map.put("영희나이", 21);
-
     return map;
   }
-
-
   @RequestMapping("/usr/home/getArticle")
   @ResponseBody
   public Article getArticle() {
-    Article article = new Article(1, "제목1");
+    Article article = new Article(1, "제목1", "내용1");
 
     return article;
   }
@@ -93,30 +63,19 @@ public class UsrHomeController {
   @RequestMapping("/usr/home/getArticles")
   @ResponseBody
   public List<Article> getArticles() {
-    Article article1 = new Article(1, "제목1");
-    Article article2 = new Article(2, "제목2");
+    Article article1 = new Article(1, "제목1", "내용1");
+    Article article2 = new Article(2, "제목2", "내용2");
 
     List<Article> list = new ArrayList<>();
     list.add(article1);
     list.add(article2);
-
     return list;
   }
-
   @RequestMapping("/usr/home/doSetCount")
   @ResponseBody
   public String doSetCount(int count) {
     this.count = count;
     return "count의 값이 " + this.count + "으로 초기화 되었습니다.";
   }
-
-}
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-class Article {
-  public int id;
-  public String title;
 
 }
