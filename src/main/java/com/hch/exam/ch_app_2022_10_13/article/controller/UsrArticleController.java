05ce7f838aa3c626e8dc46cf83d161055f0a602b
hwnagchangho <changho3755@gmail.com>
2022-11-15 00:37:49 +0900

    28강, 게시물 수정 액션

--- src/main/java/com/hch/exam/ch_app_2022_10_13/article/controller/UsrArticleController.java
+++ src/main/java/com/hch/exam/ch_app_2022_10_13/article/controller/UsrArticleController.java
@@ -34,6 +34,7 @@ public class UsrArticleController {
   private Article getArticle (int id){
     for( Article article : articles){
       if(article.getId() == id){
+//     롬복을 적용했기 때문에 getId를 써준다.
         return article;
       }
     }
@@ -55,6 +56,13 @@ public class UsrArticleController {
     Article article = getArticle(id);
 
     articles.remove(article);
+  }
+
+  private void modifyArticle(int id, String title, String body) {
+    Article article = getArticle(id);
+
+    article.setTitle(title);
+    article.setBody(body);
   }
 
   // 서비스 메서드 끝
@@ -87,6 +95,20 @@ public class UsrArticleController {
     deleteArticle(id);
 
     return id + "번 게시물이 삭제되었습니다.";
+  }
+
+  @RequestMapping("/usr/article/doModify")
+  @ResponseBody
+  public String doModify (int id, String title, String body){
+    Article article = getArticle(id);
+
+    if( article == null) {
+      return id + "번 게시물이 존재하지 않습니다.";
+    }
+
+    modifyArticle(id, title, body);
+
+    return id + "번 게시물이 수정되었습니다.";
   }
   // 액션 메서드 끝
 }
