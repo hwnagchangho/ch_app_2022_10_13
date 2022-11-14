206cea3df566477533e9500959da8cc3059c01e9
hwnagchangho <changho3755@gmail.com>
2022-11-14 23:49:38 +0900

    26강, writeArticle 메서드를 통해서 게시물 작성로직 중복제거

--- src/main/java/com/hch/exam/ch_app_2022_10_13/article/controller/UsrArticleController.java
+++ src/main/java/com/hch/exam/ch_app_2022_10_13/article/controller/UsrArticleController.java
@@ -23,25 +23,28 @@ public class UsrArticleController {
 
   private void makeTestData() {
     for(int i = 1; i <=10; i++){
-      int id = articleLastId + 1;
       String title = "제목" + i;
       String body = "내용" + i;
 
-      Article article = new Article(id, title, body);
-
-      articles.add(article);
-      articleLastId = id;
+      writeArticle(title, body);
     }
+  }
+
+  private Article writeArticle(String title, String body) {
+    int id = articleLastId + 1;
+
+    Article article = new Article(id, title, body);
+
+    articles.add(article);
+    articleLastId = id;
+
+    return article;
   }
 
   @RequestMapping("/usr/article/doAdd")
   @ResponseBody
   public Article doAdd (String title, String body){
-    int id = articleLastId + 1;
-    Article article = new Article(id, title, body);
-
-    articles.add(article);
-    articleLastId = id;
+    Article article = writeArticle(title, body);
 
     return article;
   }
