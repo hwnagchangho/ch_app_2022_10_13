de26bfad4f5b8c52982a5c8c5ea5b0ce83cc6b4b
hwnagchangho <changho3755@gmail.com>
2022-11-19 09:33:58 +0900

    51강, 회원가입 시, 입력데이터 유효성 체크, 공백도 체크

--- src/main/java/com/hch/exam/ch_app_2022_10_13/member/controller/UsrMemberController.java
+++ src/main/java/com/hch/exam/ch_app_2022_10_13/member/controller/UsrMemberController.java
@@ -20,6 +20,30 @@ public class UsrMemberController {
   public Object doJoin (String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email){
     int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 
+    if( loginId == null || loginId.trim().length() == 0) {
+      return "loginId(을)를 입력 해주세요.";
+    }
+
+    if( loginPw == null || loginPw.trim().length() == 0 ) {
+      return "loginPw(을)를 입력 해주세요.";
+    }
+
+    if( name == null || name.trim().length() == 0 ) {
+      return "name(을)를 입력 해주세요.";
+    }
+
+    if( nickname == null || nickname.trim().length() == 0) {
+      return "nickname(을)를 입력 해주세요.";
+    }
+
+    if( cellphoneNo == null || cellphoneNo.trim().length() == 0) {
+      return "cellphoneNo(을)를 입력 해주세요.";
+    }
+
+    if( email == null || email.trim().length() == 0) {
+      return "email(을)를 입력 해주세요.";
+    }
+
     Member member  = memberService.getMemberById(id);
 
     if(id == -1 ){
