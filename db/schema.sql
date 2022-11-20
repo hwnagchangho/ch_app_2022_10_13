ad3939e4b741fe8ef354e6cd950678dfdc802817
hwnagchangho <changho3755@gmail.com>
2022-11-20 21:02:19 +0900

    72강, 게시물 삭제시 권한체크

--- db/schema.sql
+++ db/schema.sql
@@ -84,3 +84,8 @@ email = "chang2210@naver.com";
 
 #게시물 테이블에 회원번호칼럼 추가
 ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
+
+# 기존 게시물의 작성자를 2번으로 지정
+UPDATE article
+SET memberId = 2
+WHERE memberId = 0;
