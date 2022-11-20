50dcf083bdce0f748fbd3a7600309450717f8716
hwnagchangho <changho3755@gmail.com>
2022-11-20 22:44:50 +0900

    77강, JSP 적용

--- src/main/java/com/hch/exam/ch_app_2022_10_13/controller/UsrHomeController.java
+++ src/main/java/com/hch/exam/ch_app_2022_10_13/controller/UsrHomeController.java
@@ -2,13 +2,16 @@ package com.hch.exam.ch_app_2022_10_13.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
    @RequestMapping("/usr/home/main")
-  @ResponseBody
-  public String main() {
-    return "안녕";
+  public String showMain() {
+    return "/usr/home/main";
+  }
+
+  @RequestMapping("/")
+  public String showRoot() {
+    return "redirect:/usr/home/main";
   }
 }
