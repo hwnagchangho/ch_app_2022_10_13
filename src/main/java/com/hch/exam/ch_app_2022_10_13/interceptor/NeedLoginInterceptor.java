ff18f1b5dff7b701515eb1bf6e7894f9a157d850
hwnagchangho <changho3755@gmail.com>
2022-11-22 12:21:34 +0900

    97강, NeedLogin인터셉터 구현 및 등록

---
+++ src/main/java/com/hch/exam/ch_app_2022_10_13/interceptor/NeedLoginInterceptor.java
@@ -0,0 +1,17 @@
+package com.hch.exam.ch_app_2022_10_13.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class NeedLoginInterceptor implements HandlerInterceptor {
+  @Override
+  public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handle) throws Exception {
+    System.out.println("로그인 필요");
+
+    return HandlerInterceptor.super.preHandle(req, resp, handle);
+  }
+}
