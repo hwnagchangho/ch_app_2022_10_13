3f21e215758084ad5b0331e84164c725061aa7cd
hwnagchangho <changho3755@gmail.com>
2022-11-26 21:40:38 +0900

    111강, 멀티게시판 개념 도입, board 테이블 생성

--- db/schema.sql
+++ db/schema.sql
@@ -33,12 +33,13 @@ title = "제목3",
 SELECT LAST_INSERT_ID();
 
 
+
 # 회원 테이블 생성
 CREATE TABLE `member`(
 	id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	regDate DATETIME NOT NULL,
 	updateDate DATETIME NOT NULL,
-	loginId CHAR(20) UNIQUE NOT NULL,
+	loginId CHAR(20) NOT NULL,
 	loginPw CHAR(60) NOT NULL,
 	`authLevel` SMALLINT(2) UNSIGNED DEFAULT 3 COMMENT '권한레벨(3=일반, 7=관리자)',
 	`name` CHAR(20) NOT NULL,
@@ -89,3 +90,40 @@ ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDa
 UPDATE article
 SET memberId = 2
 WHERE memberId = 0;
+
+# 게시판 테이블 생성
+CREATE TABLE board(
+	id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME NOT NULL,
+	updateDate DATETIME NOT NULL,
+	`code` CHAR(50) NOT NULL UNIQUE COMMENT'notice=공지사항, free=자유게시판, free1=자유게시판2',
+	`name` CHAR(60) NOT NULL UNIQUE COMMENT'게시판 이름',
+	delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '삭제여부(0=삭제전, 1=삭제)',
+	delDate DATETIME COMMENT '삭제날짜'
+);
+
+# 기본 게시판 생성
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'notice',
+`name` = '공지사항';
+
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'free',
+`name` = '자유게시판';
+
+# 게시판 테이블에 boardId 칼럼 추가
+ALTER TABLE article ADD COLUMN boardId INT(10) UNSIGNED NOT NULL AFTER memberId;
+
+# 1, 2번 게시물을 공지사항 게시물로 지정
+UPDATE article
+SET boardId = 1
+WHERE id IN(1,2);
+
+# 3번 게시물을 자유게시판 게시물로 지정
+UPDATE article
+SET boardId = 2
+WHERE id IN(3);
